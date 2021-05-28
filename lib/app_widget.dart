2630e890fc63841bc2d87e6826719747f9572820
Wellington Prado <well.cp@hotmail.com>
2021-05-28 07:49:43 -0300

    Firebase install

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,53 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/login/login_page.dart';
-import 'package:split_it/modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
+  @override
+  _AppWidgetState createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      return Material(
+        child: Center(
+          child: Text("Erro na inicialização do Firebase"),
+        ),
+      );
+    }
+
+    if (!_initialized) {
+      return Material(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Split.it",
