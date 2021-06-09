bfae643db5167bcbbd6c7a929d7553a58a7772a8
well-prado <globaldesksolutions@gmail.com>
2021-06-09 18:54:38 -0300

    Updating MOBX 06-09-2021

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,36 +1,29 @@
-import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
 import 'package:split_it/modules/login/login_service.dart';
 import 'package:split_it/modules/login/login_state.dart';
 
 class LoginController {
-  LoginState state = LoginStateEmpty();
-  VoidCallback onUpdate;
-  Function(LoginState state)? onChange;
+  final _state = Observable<LoginState>(LoginStateEmpty());
+  LoginState get state => _state.value;
+
+  set state(LoginState state) => _state.value = state;
 
   final LoginService service;
-  LoginController({required this.onUpdate, required this.service});
+  final _actionController = ActionController();
+
+  LoginController({required this.service});
 
   Future<void> googleSignIn() async {
+    _actionController.startAction(name: "LoginController.googleSignIn");
+    final startTime = DateTime.now();
     try {
       state = LoginStateLoading();
-      update();
       final user = await service.googleSignIn();
       state = LoginStateSuccess(user: user);
-      update();
     } catch (error) {
       state = LoginStateFailure(message: error.toString());
-      update();
     }
-  }
-
-  void update() {
-    onUpdate();
-    if (onChange != null) {
-      onChange!(state);
-    }
-  }
-
-  void listen(Function(LoginState state) onChange) {
-    this.onChange = onChange;
+    _actionController.endAction(ActionRunInfo(
+        name: "LoginController.googleSignIn", startTime: startTime));
   }
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:split_it/modules/login/login_controller.dart';
 import 'package:split_it/modules/login/login_service.dart';
@@ -19,15 +21,14 @@ class _LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     controller = LoginController(
-        service: LoginServiceImpl(),
-        onUpdate: () {
-          if (controller.state is LoginStateSuccess) {
-            final user = (controller.state as LoginStateSuccess).user;
-            Navigator.pushReplacementNamed(context, "/home", arguments: user);
-          } else {
-            setState(() {});
-          }
-        });
+      service: LoginServiceImpl(),
+    );
+    autorun((_) {
+      if (controller.state is LoginStateSuccess) {
+        final user = (controller.state as LoginStateSuccess).user;
+        Navigator.pushReplacementNamed(context, "/home", arguments: user);
+      }
+    });
     super.initState();
   }
 
@@ -67,21 +68,23 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               SizedBox(height: 32),
-              if (controller.state is LoginStateLoading) ...[
-                CircularProgressIndicator(),
-              ] else if (controller.state is LoginStateFailure) ...[
-                Text((controller.state as LoginStateFailure).message)
-              ] else
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 48),
-                  child: SocialButtonWidget(
-                    imagePath: "assets/images/google_icon.png",
-                    label: "Entrar com Google",
-                    onTap: () {
-                      controller.googleSignIn();
-                    },
-                  ),
-                ),
+              Observer(builder: (context) {
+                if (controller.state is LoginStateLoading)
+                  return CircularProgressIndicator();
+                else if (controller.state is LoginStateFailure)
+                  return Text((controller.state as LoginStateFailure).message);
+                else
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 48),
+                    child: SocialButtonWidget(
+                      imagePath: "assets/images/google_icon.png",
+                      label: "Entrar com Google",
+                      onTap: () {
+                        controller.googleSignIn();
+                      },
+                    ),
+                  );
+              }),
               SizedBox(height: 16),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 48),
